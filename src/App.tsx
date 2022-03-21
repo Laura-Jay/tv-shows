5a1a66b614083a050f271f5f2445c83a9f3f60e1
Tiffany Wilmot-Simpson <twilmotsimpson@gmail.com>
2022-03-21 08:17:19 -0500

    added data file and tested it works

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import { greet } from "./utils/greet";
+import episodes from "./data/episodes.json";
 
 function App(): JSX.Element {
+  console.log("Imported, ", episodes.length, "episode(s)");
+  console.log("First episodes name is ", episodes[0].name);
+
   return <h1>{greet("World")}</h1>;
 }
 
