9cab802a4d4c9c04b83edc88e07c9407e8edbfaf
laura J <laura@academy.com>
2022-03-22 17:26:09 +0000

    formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-
 // import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
-
 import "./styles.css";
 import MainContent from "./MainContent";
-
 
 function App(): JSX.Element {
   return (
