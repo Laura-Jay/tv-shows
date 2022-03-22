703426aa6422f7172e540e10417463b29338861e
Tiffany Wilmot-Simpson <twilmotsimpson@gmail.com>
2022-03-22 07:35:00 -0500

    fixed previous check errors

--- src/components/FormattingSeasonAndEpisode.tsx
+++ src/components/FormattingSeasonAndEpisode.tsx
@@ -1,6 +1,6 @@
 export default function FormattSeason(season: number, number: number): string {
-  let seasonFormatted = season.toString().padStart(2, "0");
-  let episodeFormatted = number.toString().padStart(2, "0");
+  const seasonFormatted = season.toString().padStart(2, "0");
+  const episodeFormatted = number.toString().padStart(2, "0");
 
   return `S${seasonFormatted}E${episodeFormatted}`;
 }
