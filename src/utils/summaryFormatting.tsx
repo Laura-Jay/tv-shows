52eb62922fb7af6b4461527e0cf51e313ee1977c
laura J <laura@academy.com>
2022-03-24 13:18:44 +0000

    fetching added

--- src/utils/summaryFormatting.tsx
+++ src/utils/summaryFormatting.tsx
@@ -1,7 +1,7 @@
 export default function summaryFormatting(summary: string): string {
   if (summary) {
-    let remove = summary.replace(/<\/?p[^>]*>/g, " ");
-    remove = remove.replace(/<\/?b[^>]*>/g, " ");
+    let remove = summary.replace(/<\/?p[^>]*>/g, "");
+    remove = remove.replace(/<\/?b[^>]*>/g, "");
     return remove.replace(/<\/?i[^>]*>/g, " ");
   } else {
     return "No summary found";
