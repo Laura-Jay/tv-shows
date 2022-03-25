f217269d982eeac30cb8183d134dc9cac49d5ef3
laura J <laura@academy.com>
2022-03-25 15:01:34 +0000

    added css

--- src/components/TvShow.tsx
+++ src/components/TvShow.tsx
@@ -13,18 +13,23 @@ interface TvShowProps {
 export default function TvShow(props: TvShowProps): JSX.Element {
   return (
     <section className="tv-show-list-item" onClick={props.handleClick}>
-      <h2 className="show--name">{props.name}</h2>
-
-      <img
-        src={props.image}
-        alt="show screen capture"
-        className="show--image"
-      />
-      <p>{props.genres}</p>
-      <p>{props.summary}</p>
-      <p>{props.status}</p>
-      <p>{props.rating}</p>
-      <p>{props.runtime}</p>
+      <div className="tv-show-hero">
+        <h2 className="show--name">{props.name}</h2>
+        <img
+          src={props.image}
+          alt="show screen capture"
+          className="show--image"
+        />
+      </div>
+      <p className="tv-show-summary">Summary: {props.summary}</p>
+      <div className="tv-show-list-stats">
+        <ul>
+          <li>Genres: {props.genres}</li>
+          <li>Status: {props.status}</li>
+          <li>Rating: {props.rating}</li>
+          <li>Runtime: {props.runtime}</li>
+        </ul>
+      </div>
     </section>
   );
 }
